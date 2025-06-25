8a3377579fd6fdd9e248c95af0bccd2c468f98fa
Indrawan <indrawanismail0@gmail.com>
2025-06-25 23:23:08 +0800

    update

---
+++ App.jsx
@@ -0,0 +1,13 @@
+/**
+ * Challenge: Build the Header component in a separate file
+ * and render it here in the App component
+ */
+import Header from "./Header"
+
+export default function App() {
+    return (
+        <>
+        <Header />
+        </>
+    )
+}
---
+++ Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header className="nav-container">
+            <nav className="nav-wrapper">
+                <img className="nav-logo" src="/images/chefNime2.png" alt="chef" />
+                <h1 className="nav-text">Chef Nime</h1>
+            </nav>
+            </header>
+    )
+}
